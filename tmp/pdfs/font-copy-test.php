9a978637d7a63329bb1643fc65efb84600f9a12b
Hamza-92 <ameerhamza92099@gmail.com>
2026-09-25 09:43:59 +0500

    dd

--- tmp/pdfs/font-copy-test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require dirname(__DIR__, 2).'/vendor/autoload.php';
-
-$pdf = new Mpdf\Mpdf(['tempDir' => dirname(__DIR__, 2).'/storage/app/private/paper-pdfs/mpdf']);
-$pdf->WriteHTML('<div style="font-family:dejavusans;direction:rtl">معروضی سوالات کی جوابی شیٹ</div>');
-$pdf->Output(__DIR__.'/dejavu.pdf', 'F');
